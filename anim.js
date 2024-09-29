1e558b0469354fd3b465de8c7f8a26de59b153e1
ImSleepy7 <168106439+ImSleepy7@users.noreply.github.com>
2024-09-29 18:50:05 -0500

    anim.js

--- anim.js
+++ anim.js
@@ -30,8 +30,8 @@ var lyricsData = [
   { text: "Nice butterflies in my hands", time: 176 },
   { text: "Too much light for twilight", time: 183 },
   { text: "In the mood for the flowers", time: 188 },
-  { text: "Love.", time: 140 },
-];*/
+  { text: "Love.", time: 140 },*/
+];
 
 // Animar las letras
 function updateLyrics() {
